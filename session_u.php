7388837c32f097de3ba7f70787681d323f57caef
saransh0710 <49188257+saransh0710@users.noreply.github.com>
2019-05-11 16:24:01 +0530

    Add files via upload

---
+++ session_u.php
@@ -0,0 +1,20 @@
+<?php
+// mysqli_connect() function opens a new connection to the MySQL server.
+require 'connection.php';
+$conn = Connect();
+
+session_start();// Starting Session
+
+// Storing Session
+$user_check=$_SESSION['login_user2'];
+$_SESSION["order_count"]=0;
+$_SESSION_["empty_count"]=0;
+
+// SQL Query To Fetch Complete Information Of User
+$query = "SELECT username FROM CUSTOMER WHERE username = '$user_check'";
+$ses_sql = mysqli_query($conn, $query);
+$row = mysqli_fetch_assoc($ses_sql);
+$login_session =$row['username'];
+
+
+?>
